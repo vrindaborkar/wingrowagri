596521298e72d70fd8c0feff32eb66cde4f80618
Chaitanya Sase <chaitanya2sase@gmail.com>
2022-09-28 11:58:58 +0530

    changes

--- wingrowagri-main/client/src/pages/admin/Admin.js
+++ wingrowagri-main/client/src/pages/admin/Admin.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import { Route, Routes } from "react-router-dom";
 import Adminpage from './Adminpage';
-import Adminlogin from './adminlogin';
+import Adminlogin from './Adminlogin';
+
 
 const Admin = () => {
   return (
